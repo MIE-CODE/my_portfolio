53ecbc0d90b1ea57b735037369a8f247b8fd7fe4
MIE <israelvictor126@gmail.com>
2026-05-23 00:57:55 +0100

    feat: enhance ServicesList component and update global styles
    - Updated the ServicesList component to use a new class for service tags, improving consistency in styling.
    - Added new styles for the service card tags in globals.css to support various themes and modes, enhancing visual appearance and responsiveness.
    - Included new app icons in the root layout for better PWA integration.

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,20 @@ export default function RootLayout({
     >
       <head>
         <meta name="apple-mobile-web-app-title" content="MIE" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
 
         <script
           dangerouslySetInnerHTML={{
